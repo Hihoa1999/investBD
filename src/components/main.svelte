801e00e51efc7c92f540b7024c84f71d14181cfd
Hoà Nguyễn Chí <hoatrongkylovestory1999@gmail.com>
2021-10-01 10:56:54 +0700

    update ui

--- src/components/main.svelte
+++ src/components/main.svelte
@@ -33,7 +33,7 @@
         <a href="#thongtin" class="menu__links-link">Thông tin đầu tư</a>
       </li>
       <li class="menu__links-wrapper">
-        <a href="#cohoi" class="menu__links-link">Cơ hội đầu tư</a>
+        <a href="#home__opportunity" class="menu__links-link">Cơ hội đầu tư</a>
       </li>
       <li class="menu__links-wrapper">
         <a href="#hoidap" class="menu__links-link">Hỏi đáp</a>
@@ -65,7 +65,7 @@
 
   <section class="home__introduce" id="home__introduce">
     <div class="home__introduce-potentials">
-      <div class="home__introduce-potentials-name">
+      <div class="home__introduce-potentials-block">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           class="h-5 w-5"
@@ -84,7 +84,7 @@
           efficiency of the interactive services detection the irrelevance.
         </p>
       </div>
-      <div class="home__introduce-potentials-name">
+      <div class="home__introduce-potentials-block">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z" />
         </svg>
@@ -94,7 +94,7 @@
           efficiency of the interactive services detection the irrelevance.
         </p>
       </div>
-      <div class="home__introduce-potentials-name">
+      <div class="home__introduce-potentials-block">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z" />
         </svg>
@@ -104,7 +104,7 @@
           efficiency of the interactive services detection the irrelevance.
         </p>
       </div>
-      <div class="home__introduce-potentials-name">
+      <div class="home__introduce-potentials-block">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8" />
         </svg>
@@ -116,4 +116,37 @@
       </div>
     </div>
   </section>
+
+  <section class="home__opportunity" id="home__opportunity">
+    <div class="home__opportunity-opportunitys">
+     <div class="home__opportunity-opportunitys-title">
+       <div class="home__opportunity-opportunitys-title-color">
+         
+       </div>
+       <div class="home__opportunity-opportunitys-title-text">
+         <span>Cơ hội đầu tư</span>
+      </div>
+     </div>
+      <div class="home__opportunity-opportunitys-industrialArea">
+        <div class="home__opportunity-opportunitys-industrialArea-Area">
+          <div class="industrialArea-Block1">
+
+          </div>
+          <div class="industrialArea-Block2">
+
+          </div>
+          <div class="industrialArea-Block3">
+
+          </div>
+          <div class="industrialArea-Block4">
+
+          </div>
+        </div>
+        <div class="home__opportunity-opportunitys-industrialArea-Banner">
+          
+        </div>
+      </div>
+    </div>
+  </section>
 </div>
+
